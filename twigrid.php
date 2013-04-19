c7643b5323633ff0f6539e5ed88a0dff12cd139d
Petr Kessler <kesspess@gmail.com>
2013-04-19 23:55:03 +0200

    simple loader added

---
+++ twigrid.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/TwiGrid/DataGrid/DataGrid.php';
+require_once __DIR__ . '/TwiGrid/DataGrid/Column.php';
+require_once __DIR__ . '/TwiGrid/DataGrid/Action.php';
+require_once __DIR__ . '/TwiGrid/DataGrid/RowAction.php';
+require_once __DIR__ . '/TwiGrid/DataGrid/Record.php';
+require_once __DIR__ . '/TwiGrid/DataGrid/Helpers.php';
+require_once __DIR__ . '/TwiGrid/Forms/Form.php';
+require_once __DIR__ . '/TwiGrid/Forms/PrimaryCheckbox.php';
+require_once __DIR__ . '/TwiGrid/Forms/PrimarySubmitButton.php';
